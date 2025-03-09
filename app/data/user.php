b63a4943130f78403a4337adbaec513d33f6d839
Raymond1206 <raycer1206@gmail.com>
2025-03-09 20:11:09 +0900

    Delete app/data directory

--- app/data/user.php
+++
@@ -1,8 +0,0 @@
-<?php require_once(__DIR__.'../../api/general/path.php');
-require_once(CONFIG_DIR.'/database_process.php');
-
-$usrnm = str_rot13('raymond1206');
-$pswrd = password_hash('$daisuki0311', PASSWORD_DEFAULT);
-$query = new DatabaseStatement("INSERT INTO user (username, password) VALUES (:username, :password)");
-$result = $query->Operation([':username' => $usrnm, ':password' => $pswrd]);
-echo $result;
